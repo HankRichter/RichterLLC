2b4006968fce4e0eb33084134e33a5ed6d68df6c
Hank Richter <hankrichter97@gmail.com>
2023-06-02 20:59:40 -0400

    Index.js and App.jsx set up for components.

---
+++ app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import useCarousel from "./components/Carousel";
+
+const App = () => {
+  return(
+    <>
+    <Navbar />
+    <useCarousel />
+    </>
+  )
+};
+
+export default App;
