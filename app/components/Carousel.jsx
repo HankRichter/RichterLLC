8526c70d12425f86de7c4b4cc22cad7e8e18533f
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-04 21:13:42 -0400

    Added the Carousel component

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,42 +1,12 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import useCarousel from "../hooks/useCarousel";
 
-function getVisibleItems(items, index, number) {
-    return items.slice(index, index + number);
-}
+const Carousel = () => {
+  const { visibleItems, next, prev, visibleIndex } = useCarousel(
+    yourItemsArrayHere,
+    1,
+    true
+  );
 
-function useCarousel(items, visibleNumber, wrapAround) {
-    const maxIndex = items.length - visibleNumber;
-    const minIndex = 0;
-    const [visibleIndex, setVisibleIndex] = useState(0);
-    const [visibleItems, setVisibleItems] = useState(getVisibleItems(items, 0, visibleNumber));
-    const [isFirst, setIsFirst] = useState(true);
-    const [isLast, setIsLast] = useState(visibleIndex == maxIndex);
-
-    function next() {
-        let nextIndex = visibleIndex + 1;
-        if (nextIndex > maxIndex) {
-            nextIndex = wrapAround ? minIndex : maxIndex;
-        }
-        setVariables(nextIndex);
-    }
-
-    function prev() {
-        let nextIndex = visibleIndex - 1;
-        if (nextIndex < minIndex) {
-            nextIndex = wrapAround ? maxIndex : 0;
-        }
-        setVariables(nextIndex);
-    }
-
-    function setVariables(index) {
-        setVisibleIndex(index);
-        setVisibleItems(getVisibleItems(items, index, visibleNumber));
-        setIsFirst(index === minIndex);
-        setIsLast(index === maxIndex);
-    }
-
-    return { visibleItems, next, prev, isFirst, isLast, visibleIndex };
-}
-
-export default useCarousel;
+  return;
+};
