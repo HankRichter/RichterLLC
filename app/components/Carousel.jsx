662a17681460a0f5ecdd0bdf9f8be491393b1741
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-11-17 13:59:31 -0500

    Created two carousels. One houses the images and the other houses the images and descriptions. Need data to populate.

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,12 +1,71 @@
 import React from "react";
 import useCarousel from "../hooks/useCarousel";
 
-const Carousel = () => {
+const yourItemsArrayHere = [
+  {
+    name: "Object 1",
+    images: ["image1.jpg", "image2.jpg", "image3.jpg"],
+    description: "Description 1",
+  },
+  {
+    name: "Object 2",
+    images: ["image4.jpg", "image5.jpg", "image6.jpg"],
+    description: "Description 2",
+  },
+  {
+    name: "Object 3",
+    images: ["image7.jpg", "image8.jpg", "image9.jpg"],
+    description: "Description 3",
+  },
+];
+
+const ImageCarousel = ({ images }) => {
   const { visibleItems, next, prev, visibleIndex } = useCarousel(
-    yourItemsArrayHere,
+    images,
     1,
     true
   );
 
-  return;
+  return (
+    <div>
+      <button onClick={prev}>Previous</button>
+      {visibleItems.map((image, index) => (
+        <img key={index} src={image} alt={`Image ${index + 1}`} />
+      ))}
+      <button onClick={next}>Next</button>
+    </div>
+  );
 };
+
+const CombinedCarousel = ({ items }) => {
+  const { visibleItems, next, prev, visibleIndex } = useCarousel(
+    items,
+    1,
+    true
+  );
+
+  return (
+    <div>
+      <button onClick={prev}>Previous</button>
+      {visibleItems.map((item, index) => (
+        <div key={index}>
+          <h3>{item.name}</h3>
+          <ImageCarousel images={item.images} />
+          <p>{item.description}</p>
+        </div>
+      ))}
+      <button onClick={next}>Next</button>
+    </div>
+  );
+};
+
+const Carousel = () => {
+  return (
+    <div>
+      <h2>Combined Carousel</h2>
+      <CombinedCarousel items={yourItemsArrayHere} />
+    </div>
+  );
+};
+
+export default Carousel;
