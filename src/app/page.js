b05ce364e57d104fed39e6a37f44ff943f61df09
Hank Richter <hankrichter97@gmail.com>
2026-05-25 10:43:22 -0400

    Text update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="container mx-auto text-center py-20">
-      <h1 className="text-5xl font-bold mb-6">Welcome to Richter Restorations</h1>
+      <h1 className="text-5xl font-bold mb-6">Welcome to Richter's Restorations</h1>
       <Image
         src="/Main/MainPhoto.jpeg"
         alt="Hank, Gus and Eddie"
